b4eb70eb25d1a46813e7ee3e056c096398788ab7
MykolaMelnyk28 <lemone681@gmail.com>
2023-11-26 00:13:05 +0200

    Create page routes list
    - Create directory pages
    - Add new Route objects to array in app-routing.module.ts
    ----{ path: '', redirectTo: 'home', pathMatch: 'full'},
    ----{ path: 'home', component: AppComponent },
    ----{ path: 'services', component: AppComponent },
    ----{ path: 'gallery', component: AppComponent },
    ----{ path: 'aboutus', component: AppComponent },
    ----{ path: 'feedbacks', component: AppComponent },
    ----{ path: 'appointment', component: AppComponent }

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: AppComponent },
+  { path: 'services', component: AppComponent },
+  { path: 'gallery', component: AppComponent },
+  { path: 'aboutus', component: AppComponent },
+  { path: 'feedbacks', component: AppComponent },
+  { path: 'appointment', component: AppComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
