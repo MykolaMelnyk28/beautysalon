675f7d0490e7bb5e67a9af2190eac4e5de5cf6e4
MykolaMelnyk28 <lemone681@gmail.com>
2023-12-24 01:31:31 +0200

    Create component FeedbacksPage

--- frontend/src/app/pages/feedbacks-page/feedbacks-page.component.ts
+++ frontend/src/app/pages/feedbacks-page/feedbacks-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {FeedbackModel} from "../../../model/FeedbackModel";
+import {CreateFeedbackModel} from "../../components/feedbacks/create-feedback-dialog/CreateFeedbackModel";
 
 @Component({
   selector: 'app-feedbacks-page',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./feedbacks-page.component.css']
 })
 export class FeedbacksPageComponent {
+  feedbacks: FeedbackModel[];
 
+  isDialogVisible: boolean = false;
+
+  constructor() {
+    this.feedbacks = [];
+  }
+
+  openDialog() {
+    this.isDialogVisible = true;
+  }
+
+  onDialogClose() {
+    this.isDialogVisible = false;
+  }
+
+  onDialogSubmit(reviewData: CreateFeedbackModel) {
+    //let f = {};
+    //this.feedbacks.push(f);
+  }
 }
