d95613108eaee175854164b2a47ddecc7306acb3
MykolaMelnyk28 <lemone681@gmail.com>
2023-12-25 14:16:23 +0200

    Modify component ServicesPage

--- frontend/src/app/pages/services-page/services-page.component.ts
+++ frontend/src/app/pages/services-page/services-page.component.ts
@@ -1,4 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TreeNode} from "../../../model/TreeNode";
+import {EmployeeModel} from "../../../model/EmployeeModel";
+import {ServiceModel} from "../../../model/ServiceModel";
+import {ServiceEntityService} from "../../services/service-entity.service";
 
 @Component({
   selector: 'app-services-page',
@@ -6,5 +10,38 @@ import {Component} from '@angular/core';
   styleUrls: ['./services-page.component.css']
 })
 export class ServicesPageComponent {
+  isDialogVisible: boolean = true;
+  selectedService: ServiceModel;
 
+  treeRoot: TreeNode | null;
+
+  constructor(
+    private serviceService: ServiceEntityService
+  ) {
+
+    this.selectedService = new ServiceModel({});
+    this.treeRoot = this.serviceService.getTree();
+  }
+
+  hasContent(): boolean {
+    return this.treeRoot !== null;
+  }
+
+  onLeafNodeClick(leafNode: TreeNode) {
+    let s: any = this.serviceService.findByNode(leafNode);
+    if (s) {
+      this.selectedService = s;
+    } else {
+      this.selectedService = new ServiceModel({});
+    }
+    this.openServiceDialog();
+  }
+
+  openServiceDialog() {
+    this.isDialogVisible = true;
+  }
+
+  onDialogClose() {
+    this.isDialogVisible = false;
+  }
 }
