1d7f53934e17d0f4aab95e7f667b45274225f4cd
MykolaMelnyk28 <lemone681@gmail.com>
2024-04-21 17:23:45 +0300

    Create backend-ui project

---
+++ backend/backend-ui/src/main/java/com/beautysalon/ui/BackendUiApplication.java
@@ -0,0 +1,13 @@
+package com.beautysalon.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendUiApplication.class, args);
+    }
+
+}
